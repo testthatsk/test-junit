da486f32b3a66ad13aeb34acbaca9b055269bd91
Kunalsing Thakur <kunalsing.thakur@xperi.com>
2024-06-10 17:24:45 +0530

    Adding test mock

---
+++ src/test/java/com/example/ExampleTest.java
@@ -0,0 +1,308 @@
+package com.example;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ExampleTest {
+
+    @Test
+    public void test1() { assertTrue(true); }
+
+    @Test
+    public void test2() { assertTrue(true); }
+
+    @Test
+    public void test3() { assertTrue(true); }
+
+    @Test
+    public void test4() { assertTrue(true); }
+
+    @Test
+    public void test5() { assertTrue(true); }
+
+    @Test
+    public void test6() { assertTrue(true); }
+
+    @Test
+    public void test7() { assertTrue(true); }
+
+    @Test
+    public void test8() { assertTrue(true); }
+
+    @Test
+    public void test9() { assertTrue(true); }
+
+    @Test
+    public void test10() { assertTrue(true); }
+
+    @Test
+    public void test11() { assertTrue(true); }
+
+    @Test
+    public void test12() { assertTrue(true); }
+
+    @Test
+    public void test13() { assertTrue(true); }
+
+    @Test
+    public void test14() { assertTrue(true); }
+
+    @Test
+    public void test15() { assertTrue(true); }
+
+    @Test
+    public void test16() { assertTrue(true); }
+
+    @Test
+    public void test17() { assertTrue(true); }
+
+    @Test
+    public void test18() { assertTrue(true); }
+
+    @Test
+    public void test19() { assertTrue(true); }
+
+    @Test
+    public void test20() { assertTrue(true); }
+
+    @Test
+    public void test21() { assertTrue(true); }
+
+    @Test
+    public void test22() { assertTrue(true); }
+
+    @Test
+    public void test23() { assertTrue(true); }
+
+    @Test
+    public void test24() { assertTrue(true); }
+
+    @Test
+    public void test25() { assertTrue(true); }
+
+    @Test
+    public void test26() { assertTrue(true); }
+
+    @Test
+    public void test27() { assertTrue(true); }
+
+    @Test
+    public void test28() { assertTrue(true); }
+
+    @Test
+    public void test29() { assertTrue(true); }
+
+    @Test
+    public void test30() { assertTrue(true); }
+
+    @Test
+    public void test31() { assertTrue(true); }
+
+    @Test
+    public void test32() { assertTrue(true); }
+
+    @Test
+    public void test33() { assertTrue(true); }
+
+    @Test
+    public void test34() { assertTrue(true); }
+
+    @Test
+    public void test35() { assertTrue(true); }
+
+    @Test
+    public void test36() { assertTrue(true); }
+
+    @Test
+    public void test37() { assertTrue(true); }
+
+    @Test
+    public void test38() { assertTrue(true); }
+
+    @Test
+    public void test39() { assertTrue(true); }
+
+    @Test
+    public void test40() { assertTrue(true); }
+
+    @Test
+    public void test41() { assertTrue(true); }
+
+    @Test
+    public void test42() { assertTrue(true); }
+
+    @Test
+    public void test43() { assertTrue(true); }
+
+    @Test
+    public void test44() { assertTrue(true); }
+
+    @Test
+    public void test45() { assertTrue(true); }
+
+    @Test
+    public void test46() { assertTrue(true); }
+
+    @Test
+    public void test47() { assertTrue(true); }
+
+    @Test
+    public void test48() { assertTrue(true); }
+
+    @Test
+    public void test49() { assertTrue(true); }
+
+    @Test
+    public void test50() { assertTrue(true); }
+
+    @Test
+    public void test51() { assertTrue(true); }
+
+    @Test
+    public void test52() { assertTrue(true); }
+
+    @Test
+    public void test53() { assertTrue(true); }
+
+    @Test
+    public void test54() { assertTrue(true); }
+
+    @Test
+    public void test55() { assertTrue(true); }
+
+    @Test
+    public void test56() { assertTrue(true); }
+
+    @Test
+    public void test57() { assertTrue(true); }
+
+    @Test
+    public void test58() { assertTrue(true); }
+
+    @Test
+    public void test59() { assertTrue(true); }
+
+    @Test
+    public void test60() { assertTrue(true); }
+
+    @Test
+    public void test61() { assertTrue(true); }
+
+    @Test
+    public void test62() { assertTrue(true); }
+
+    @Test
+    public void test63() { assertTrue(true); }
+
+    @Test
+    public void test64() { assertTrue(true); }
+
+    @Test
+    public void test65() { assertTrue(true); }
+
+    @Test
+    public void test66() { assertTrue(true); }
+
+    @Test
+    public void test67() { assertTrue(true); }
+
+    @Test
+    public void test68() { assertTrue(true); }
+
+    @Test
+    public void test69() { assertTrue(true); }
+
+    @Test
+    public void test70() { assertTrue(true); }
+
+    @Test
+    public void test71() { assertTrue(true); }
+
+    @Test
+    public void test72() { assertTrue(true); }
+
+    @Test
+    public void test73() { assertTrue(true); }
+
+    @Test
+    public void test74() { assertTrue(true); }
+
+    @Test
+    public void test75() { assertTrue(true); }
+
+    @Test
+    public void test76() { assertTrue(true); }
+
+    @Test
+    public void test77() { assertTrue(true); }
+
+    @Test
+    public void test78() { assertTrue(true); }
+
+    @Test
+    public void test79() { assertTrue(true); }
+
+    @Test
+    public void test80() { assertTrue(true); }
+
+    @Test
+    public void test81() { assertTrue(false); }
+
+    @Test
+    public void test82() { assertTrue(false); }
+
+    @Test
+    public void test83() { assertTrue(false); }
+
+    @Test
+    public void test84() { assertTrue(false); }
+
+    @Test
+    public void test85() { assertTrue(false); }
+
+    @Test
+    public void test86() { assertTrue(false); }
+
+    @Test
+    public void test87() { assertTrue(false); }
+
+    @Test
+    public void test88() { assertTrue(false); }
+
+    @Test
+    public void test89() { assertTrue(false); }
+
+    @Test
+    public void test90() { assertTrue(false); }
+
+    @Test
+    public void test91() { assertTrue(false); }
+
+    @Test
+    public void test92() { assertTrue(false); }
+
+    @Test
+    public void test93() { assertTrue(false); }
+
+    @Test
+    public void test94() { assertTrue(false); }
+
+    @Test
+    public void test95() { assertTrue(false); }
+
+    @Test
+    public void test96() { assertTrue(false); }
+
+    @Test
+    public void test97() { assertTrue(false); }
+
+    @Test
+    public void test98() { assertTrue(false); }
+
+    @Test
+    public void test99() { assertTrue(false); }
+
+    @Test
+    public void test100() { assertTrue(false); }
+}
+
